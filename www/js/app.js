515c080477618dca964c96c918637ef39788bd7d
Luisita <luiyi_1414@hotmail.com>
2015-02-08 14:06:19 -0600

    5

--- www/js/app.js
+++ www/js/app.js
@@ -2,19 +2,11 @@ var ban = 0; // Bandera para que el dispositivo abra google solo una ves
 // Evento online
 $(document).on('online', function(){ 
   if (ban === 0) {
-    alert("En Línea","OnLine");
+    $.mobile.changePage("#dialogo");
     window.open('http://www.google.com.mx', '_system');
     ban = 1;
   }
 });
-function alert(txt, title) {
-    try {
-        jAlert(txt, title);
-    } catch (e) {
-        oAlert(txt);
-    }
-};
-
 // Evento offline
 $(document).on('offline', function(){ 
   if (ban === 0) {
