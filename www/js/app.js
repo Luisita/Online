b42e6bf2916eb88159de483da53953c8cc6196bc
Luisita <luiyi_1414@hotmail.com>
2015-02-08 13:46:29 -0600

    4

--- www/js/app.js
+++ www/js/app.js
@@ -2,18 +2,25 @@ var ban = 0; // Bandera para que el dispositivo abra google solo una ves
 // Evento online
 $(document).on('online', function(){ 
   if (ban === 0) {
-    alert("Estamos conectados a internet ... ");
+    alert("En Línea","OnLine");
     window.open('http://www.google.com.mx', '_system');
     ban = 1;
   }
+  function alert(txt, title) {
+    try {
+        jAlert(txt, title);
+    } catch (e) {
+        oAlert(txt);
+    }
+  };
 });
 // Evento offline
 $(document).on('offline', function(){ 
   if (ban === 0) {
     $.mobile.loading( "show", {
-          text: "No estamos conectados a internet",
+          text: "No está en línea, conectese a una red",
           textVisible: true,
-          theme: "b",
+          theme: "e",
           html: ""
       });
     ban = 1;
